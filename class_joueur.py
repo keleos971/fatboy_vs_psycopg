e4b5ea12233cefb3e1ceab1ee3c01fda792d5476
voxclam <voxclam.indes@gmail.com>
2015-11-24 10:06:11 +0100

    Retrying... fucked up last commit

--- class_joueur.py
+++ class_joueur.py
@@ -9,15 +9,13 @@ class Joueur:
     def __init__( self, recordFromDB ):
         self.id = recordFromDB[0]
         self.nom = recordFromDB[1]
-        self.personnage = recordFromDB[2]
-        self.date_inscription = recordFromDB[3]
-        self.commentaire = recordFromDB[4]
+        self.datec = recordFromDB[2]
+        self.commentaire = recordFromDB[3]
 
     # Displays the information of a Joueur
     def displayJoueur(self):
-        if self.commentaire == '':
-            # If commentaire string is empty, no need to display it
-            print("Id =", self.id, ", nom =", self.nom, ", perso =", self.personnage, ", date inscription =", self.date_inscription)
-        else:
-            print("Id =", self.id, ", nom =", self.nom, ", perso =", self.personnage, ", date inscription =", self.date_inscription, ", commentaire =", self.commentaire)
-
+            print(
+                "Id = ", self.id,
+                ", nom = ",self.nom,
+                ", date inscription =", self.datec,
+                ", commentaire = ", self.commentaire)
